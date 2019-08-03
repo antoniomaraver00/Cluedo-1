2e3dc30f61b43795b57ea6a475faff1a6a271d22
shootinSideways <ty.howard1019@hotmail.com>
2019-08-03 17:38:53 +1200

    quite a few changes reguarding movement and rounds

--- src/player/Move.java
+++ src/player/Move.java
@@ -1,7 +1,9 @@
 package player;
 
+
+
 public class Move {
-	
+	private static Grid boardGrid = new Grid();
 
 	public Move() {
 		
@@ -9,10 +11,22 @@ public class Move {
 
 	
 
-	public void apply() {
-
+	public void apply(Position p,int newY, int newX, char boardChar) {
+		boardGrid.setGridChar(p.getY(), p.getX(),newY,newX, boardChar);
+		p.setY(newY);
+		p.setX(newX);
+		boardGrid.display();
 	}
-
+	public void moveSpawnPos(int row,int col,char boardChar)  {
+		boardGrid.setGridChar(row, col, boardChar);
+	}
+	public boolean isMoveValid(int row, int col) {//if the desired coord is occupied by a underscore, its okay to move there
+		if (boardGrid.getGridChar(row, col)=='_') {return true;}
+		return false;
+	}
+	public Grid getGrid() {
+		return boardGrid;
+	}
 	
 	/**
 	public String toString() {
