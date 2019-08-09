5a109e1b4f02fc61d70bd928d68752869752a16b
Sultan <smob123@hotmail.com>
2019-08-10 10:29:05 +1200

    added character selection to the gui

--- src/game/GUIGame.java
+++ src/game/GUIGame.java
@@ -1,8 +1,12 @@
 package game;
 
 import javax.swing.*;
+
+import player.Player;
+
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 
 public class GUIGame extends JFrame {
 	JPanel panel = new JPanel();
@@ -60,24 +64,103 @@ public class GUIGame extends JFrame {
 		label.setAlignmentX(CENTER_ALIGNMENT);
 		panel.add(label);
 
-		// radio buttons for the user to choose the number of players
-		JRadioButton[] radioButtons = new JRadioButton[4];
-		ButtonGroup btnGroup = new ButtonGroup();
+		// buttons for the user to choose the number of players
+		JButton[] buttons = new JButton[4];
 
-		for (int i = 0; i < radioButtons.length; i++) {
-			radioButtons[i] = new JRadioButton();
-			radioButtons[i].setText(Integer.toString(i + 3));
-			radioButtons[i].setAlignmentX(CENTER_ALIGNMENT);
+		for (int i = 0; i < buttons.length; i++) {
+			buttons[i] = new JButton();
+			// number of players is from 3 to 6
+			buttons[i].setText(Integer.toString(i + 3));
+			// align buttons to the center of the window
+			buttons[i].setAlignmentX(CENTER_ALIGNMENT);
 
-			btnGroup.add(radioButtons[i]);
+			buttons[i].addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					// get the number on the button
+					int numOfPlayers = Integer.parseInt(e.getActionCommand());
+					// clear the graphics, and update the JFrame
+					panel.removeAll();
+					panel.updateUI();
+					// go to the character selection
+					characterSelection(numOfPlayers, 1);
+				}
+			});
 
-			panel.add(radioButtons[i]);
+			panel.add(buttons[i]);
+		}
+	}
+
+	private void characterSelection(int numOfPlayers, final int currentPlayer) {
+		// check if all players have chosen a character
+		if (numOfPlayers == 0) {
+			panel.removeAll();
+			panel.updateUI();
+			distributeCards();
+			return;
 		}
 
-		// add a button to start the game
-		JButton button = new JButton("Enter");
-		button.setAlignmentX(CENTER_ALIGNMENT);
-		panel.add(button);
+		// one radio button for each available character
+		ArrayList<JRadioButton> radioButtons = new ArrayList<>();
+		ButtonGroup group = new ButtonGroup();
+
+		// ask the current player to choose a character
+		String labelTxt = "Player " + currentPlayer + " please choose a character";
+		JLabel label = new JLabel(labelTxt);
+
+		// align the label to the center of the window and add it to the panel
+		label.setAlignmentX(CENTER_ALIGNMENT);
+		panel.add(label);
+
+		// get list of available characters
+		Player[] availablePlayers = board.getAvailablePlayers();
+
+		for (int j = 0; j < availablePlayers.length; j++) {
+			// add a radio button for each character
+			JRadioButton currentButton = new JRadioButton(board.getAvailablePlayers()[j].toString());
+
+			// align buttons to the center of the window, and them it to the panel
+			currentButton.setAlignmentX(CENTER_ALIGNMENT);
+			radioButtons.add(currentButton);
+			group.add(currentButton);
+			panel.add(currentButton);
+		}
+
+		// button to confirm character selection
+		JButton enter = new JButton("enter");
+		enter.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				for (int i = 0; i < radioButtons.size(); i++) {
+					// get the selected button
+					if (radioButtons.get(i).isSelected()) {
+						// add the character that is at the same index as the selected button
+						board.addPlayer(availablePlayers[i]);
+
+						// update graphics
+						panel.removeAll();
+						panel.updateUI();
+
+						// rerun the method for the next player to choose a character
+						int remaining = numOfPlayers - 1;
+						int nextPlayer = currentPlayer + 1;
+						characterSelection(remaining, nextPlayer);
+					}
+				}
+			}
+		});
+
+		// align the button to the center of the window
+		enter.setAlignmentX(CENTER_ALIGNMENT);
+		
+		// add the button to the panel and update graphics
+		panel.add(enter);
+
+		panel.updateUI();
+	}
+
+	private void distributeCards() {
+		board.distributeCards(board.getPlayers().size());
 	}
 
 	public static void main(String[] args) {
